2dd16c0a99d0e46e84d8ee50d3f8f3c438c525fa
Abed Murrar <abedmurrar15@gmail.com>
2018-03-12 12:39:29 +0200

    add README file]

---
+++ node/README.md
@@ -0,0 +1,31 @@
+# README #
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+* This repository is for exploring javascript without sincerely going deep in it but making the most of the important of it
+
+### What Did I Do here? ###
+* Mainly Explored JavaScript ES6
+* Started with node.js
+#### in the no bootstrap folder ####
+* Explored functions, objects and variables
+* Explored Closures
+* In addition to more conventional HTML forms and validation
+
+#### in the ajax folders ####
+* More styling techniques for forms
+* Git branching, push and pull, and other commands
+* Dealing with HTTP GET and POST requests with Ajax and jQuery with different forms of data such as XML, JSON, and text files
+* Learnt more in HTTP request/response headers and more security headers from the server
+* connect the script to apache2 Server with PHP scripts to get/write data to MySQL Server
+
+### How can it benefit anyone? ###
+* The jQuery and the HTML Form is written to be embedded simply in any HTML code
+* Data sent with post/get can simply be modified to any data required to connect to any database/server
+
+### How can it be made better? ###
+* Turn whole HTML Form into a grid system to make it more responsive without media queries
+* embed the javascript scripts in the end of the HTML pages
+* add more security to the HTTP requests
---
+++ node/sql/users.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 11, 2018 at 09:36 AM
+-- Server version: 5.7.21-0ubuntu0.17.10.1
+-- PHP Version: 7.0.25-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `asaltech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
+(2, 'john.doe', 'johndoe@mail.com', '3fbe7173e6e2a7761a64198db556f78307de9e702e28ae818e144a2501ed6d6f');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
