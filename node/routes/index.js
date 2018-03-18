6c1de56c5429ab331d288717da86538650e6bbe0
Abed Murrar <abedmurrar15@gmail.com>
2018-03-18 15:57:00 +0200

    Use ES6 Syntax, handle GET method for login,register ejs pages, and handle errors with appropriate status code

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var HttpStatus = require('http-status-codes'); //http status codes package
+const AJAX_CALL = 'XMLHttpRequest';
+
+var sess;
+// router.use(session({ secret: 'my-application' }));
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    sess = req.session;
+    var isLogged;
+    if (sess.username)
+        isLogged = true;
+    else
+        isLogged = false;
+    res.render('index', {
+        logged: isLogged
+    });
+});
+
+
+
+/* GET login page */
+router.get('/login', (req, res) => {
+    if (req.headers['x-requested-with'] && req.headers['x-requested-with'] === AJAX_CALL) {
+        res.status(HttpStatus.OK);
+
+        res.render('login');
+    } else {
+        res.status(HttpStatus.FORBIDDEN);
+        res.render('forbidden');
+    }
+    if (req.headers.origin && req.headers.origin !== DOMAIN) {
+        res.status(HttpStatus.FORBIDDEN);
+        res.render('forbidden');
+    }
+
+});
+
+/* GET register page */
+router.get('/register', (req, res) => {
+    res.setHeader('Content-Security-Policy', "script-src 'self'");
+    res.render('register');
+
 });
 
 module.exports = router;
--- node/routes/users.js
+++ node/routes/users.js
@@ -1,107 +1,139 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
 
-router.get('/:id?', function(req, res, next) {
-
-    if (req.params.id) {
-
-        User.getUserById(req.params.id,
-            data => {
-                if (data) {
-                    res.json(data);
-                } else {
-                    res.json(req.body);
+var sess;
+router.get('/:id?', (req, res, next) => {
+    sess = req.session;
+    if (sess.username) {
+        if (req.params.id) {
+            User.getUserById(req.params.id,
+                data => {
+                    if (data) {
+                        res.json(data)
+                    } else {
+                        res.json(req.body)
+                    }
+                },
+                error => {
+                    if (error) {
+                        res.json(error)
+                        res.status(error.code)
+                    } else {
+                        res.json(req.body)
+                    }
                 }
-            },
-            error => {
-                if (error) {
-                    res.json(err);
-                    res.status(error.code);
-                } else {
-                    res.json(req.body);
+            )
+        } else {
+            User.getAllUsers(
+                data => {
+                    if (data) {
+                        res.json(data)
+                    } else {
+                        res.json(req.body)
+                    }
+                },
+                error => {
+                    if (error) {
+                        res.json(error)
+                    } else {
+                        res.json(req.body)
+                    }
                 }
-            }
-        );
+            )
+        }
     } else {
+        res.render('forbidden');
+    }
 
-        User.getAllUsers(
+})
+router.post('/login', (req, res, next) => {
+    sess = req.session;
+    if (!sess.username) {
+        User.login(req.body,
             data => {
-                if (data) {
-                    res.json(data);
-                    res.status(400);
+                if (Object.keys(data[0]).length) {
+                    sess.username = data[0].username;
+                    res.redirect('http://localhost:8080')
+                    res.status(200)
+                    res.end()
                 } else {
-                    res.json(req.body);
+                    res.redirect('http://localhost:8080')
+                    res.status(400)
+                    res.end()
                 }
-
             },
             error => {
                 if (error) {
-                    res.json(error);
-                } else {
-                    res.json(req.body);
-                }
-            }
-        );
-    }
-});
-router.post('/', function(req, res, next) {
-
-    User.addUs.er(req.body,
+                    if (error.code === 'ER_DUP_ENTRY') // check ER_DUP_ENTRY
+                    { res.status(409) } else { res.status(400) }
+                    res.json(error)
+                } else { res.json(req.body) }
+            })
+    } else { res.redirect('forbidden') }
+})
+router.post('/', (req, res, next) => {
+    User.addUser(req.body,
         data => {
             if (data) {
-                res.json(data);
+                res.json(data)
             } else {
-                res.json(req.body); //or return count for 1 &amp;amp;amp; 0
+                res.json(req.body)
             }
         },
         error => {
             if (error) {
                 if (error.code === 'ER_DUP_ENTRY') // check ER_DUP_ENTRY
-                    res.status(409);
-                else
-                    res.status(400);
-                res.json(error);
-            } else
-                res.json(req.body);
+                { res.status(409) } else { res.status(400) }
+                res.json(error)
+            } else { res.json(req.body) }
         }
-    );
-});
-router.delete('/:id', function(req, res, next) {
-
-    User.deleteUser(req.params.id,
-        data => {
-            if (data) {
-                res.json(data);
-            } else {
-                res.json(req.body); //or return count for 1 &amp;amp;amp; 0
-            }
-        },
-        error => {
-            if (error) {
-                res.status(404)
-                res.json(err);
-            } else {
-                res.json(req.body);
-            }
-        });
-});
-router.put('/:id', function(req, res, next) {
-
-    User.updateUser(req.params.id, req.body,
-        data => {
-            if (data)
-                res.json(data);
-            else
-                res.json(req.body);
-        },
-        error => {
-            if (error) {
-                if (error.code)
-                    res.status(error.code);
-                res.json(error);
-            } else
-                res.json(req.body);
-        });
-});
-module.exports = router;
+    )
+})
+router.delete('/:id', (req, res, next) => {
+    sess = req.session;
+    if (sess.username) {
+        User.deleteUser(req.params.id,
+            data => {
+                if (data) {
+                    res.json(data)
+                } else {
+                    res.json(req.body)
+                }
+            },
+            error => {
+                if (error) {
+                    res.status(404)
+                    res.json(error)
+                } else {
+                    res.json(req.body)
+                }
+            })
+    } else {
+        res.render('forbidden');
+    }
+})
+router.put('/:id', (req, res, next) => {
+    sess = req.session;
+    if (sess.username) {
+        if (req.body.username && req.body.username === sess.username) {
+            User.updateUser(req.params.id, req.body,
+                data => {
+                    if (data) { res.json(data) } else { res.json(req.body) }
+                },
+                error => {
+                    if (error) {
+                        if (error.code === 'ER_DUP_ENTRY') { // check ER_DUP_ENTRY
+                            res.status(409)
+                        } else { res.status(400) }
+                        res.json(error)
+                    } else { res.json(req.body) }
+                })
+        } else {
+            res.render('forbidden');
+        }
+    } else {
+        res.render('forbidden');
+    }
+})
+module.exports = router
