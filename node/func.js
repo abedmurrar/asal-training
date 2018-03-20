16a6fae422dd91fa0295e7100b64c31fab8101be
Abed Murrar <abedmurrar15@gmail.com>
2018-03-20 14:33:34 +0200

    make one file for all local functions in project

---
+++ node/func.js
@@ -0,0 +1,9 @@
+var local = {
+    //check if user is logged from sessions
+    isLogged: req => {
+        if (req.session.username)
+            return true
+        return false
+    }
+}
+module.exports = local
