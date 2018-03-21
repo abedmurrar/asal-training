2f12efbe32fd2293bff5851a303669a26e77325f
Abed Murrar <abedmurrar15@gmail.com>
2018-03-21 14:53:47 +0200

    handle errors, authentication and redirects

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,48 +1,138 @@
 var express = require('express');
 var router = express.Router();
 var HttpStatus = require('http-status-codes'); //http status codes package
+const User = require('../models/User')
+var crypto = require('crypto') //crypto package
+var func = require('../func')
 const AJAX_CALL = 'XMLHttpRequest';
 
-var sess;
-// router.use(session({ secret: 'my-application' }));
+var session;
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    sess = req.session;
-    var isLogged;
-    if (sess.username)
-        isLogged = true;
-    else
-        isLogged = false;
-    res.render('index', {
-        logged: isLogged
-    });
+    session = req.session;
+    if (!func.isLogged(req)) {
+        res.render('index', {
+            title: 'Home',
+            logged: false,
+            page: 'login'
+        });
+    } else {
+        res.render('index', {
+            title: 'Home',
+            logged: true,
+            page: 'home',
+            username: session.username,
+            role: session.role
+        });
+    }
 });
 
 
 
 /* GET login page */
 router.get('/login', (req, res) => {
-    if (req.headers['x-requested-with'] && req.headers['x-requested-with'] === AJAX_CALL) {
-        res.status(HttpStatus.OK);
-
-        res.render('login');
+    //if user is not logged in then respond with login page
+    //if user is logged and tried to get this page then respond with home page
+    session = req.session;
+    if (!func.isLogged(req)) {
+        res.render('index', {
+            logged: false,
+            title: 'Login',
+            page: 'login'
+        })
     } else {
-        res.status(HttpStatus.FORBIDDEN);
-        res.render('forbidden');
+        res.render('index', {
+            logged: true,
+            title: 'Home',
+            page: 'home',
+            username: session.username,
+            role: session.role
+        })
     }
-    if (req.headers.origin && req.headers.origin !== DOMAIN) {
-        res.status(HttpStatus.FORBIDDEN);
-        res.render('forbidden');
-    }
-
 });
+
+/* POST to login page */
+router.post('/login', (req, res, next) => {
+    session = req.session;
+    try {
+        //check if session is not set
+        if (!session.username) {
+            User.login(req.body.username,
+                data => {
+                    passwordEncoded = crypto
+                        .createHash('sha256')
+                        .update(req.body.password)
+                        .digest('hex');
+                    //check if password entered is same as stored
+                    if (passwordEncoded == data.password) {
+                        session.username = data.username;
+                        session.role = data.role;
+                        session.id = data.id;
+                        res.status(HttpStatus.OK).json({ success: true, message: 'Successfully logged in.' })
+                    } else {
+                        res.status(HttpStatus.UNAUTHORIZED).json({ success: false, message: 'Wrong password, try again.' });
+                    }
+                },
+                error => {
+                    if (error) {
+                        res.status(HttpStatus.BAD_GATEWAY).json(error)
+                    } else { res.status(HttpStatus.BAD_GATEWAY).json(req.body) }
+                })
+        } else {
+            res.redirect('forbidden')
+        }
+    } catch (error) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).render('error', {
+            message: 'Internal error',
+            error: error
+        })
+    }
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.status(HttpStatus.OK).redirect('/')
+})
 
 /* GET register page */
 router.get('/register', (req, res) => {
-    res.setHeader('Content-Security-Policy', "script-src 'self'");
-    res.render('register');
-
+    session = req.session;
+    if (!func.isLogged(req)) {
+        res.render('index', {
+            logged: false,
+            title: 'Register',
+            page: 'register'
+        })
+    } else {
+        res.render('index', {
+            logged: true,
+            title: 'Home',
+            page: 'home',
+            username: session.username,
+            role: session.role
+        })
+    }
 });
+
+/* GET account page */
+router.get('/account', (req, res) => {
+    session = req.session;
+    if (!func.isLogged(req)) {
+        res.render('error', {
+            error: 'Page not found 404',
+            message: 'The page you requested is not found.'
+        })
+    } else {
+        res.render('index', {
+            logged: true,
+            title: 'Home',
+            page: 'account',
+            username: session.username,
+            role: session.role
+        })
+    }
+})
+
 
 module.exports = router;
--- node/routes/users.js
+++ node/routes/users.js
@@ -1,77 +1,56 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+var debug = require('debug')('users-route')
+var HttpStatus = require('http-status-codes'); //http status codes package
+var session;
 
-var sess;
 router.get('/:id?', (req, res, next) => {
-    sess = req.session;
-    if (sess.username) {
-        if (req.params.id) {
-            User.getUserById(req.params.id,
-                data => {
-                    if (data) {
-                        res.json(data)
-                    } else {
-                        res.json(req.body)
-                    }
-                },
-                error => {
-                    if (error) {
-                        res.json(error)
-                        res.status(error.code)
-                    } else {
-                        res.json(req.body)
-                    }
-                }
-            )
-        } else {
-            User.getAllUsers(
-                data => {
-                    if (data) {
-                        res.json(data)
-                    } else {
-                        res.json(req.body)
-                    }
-                },
-                error => {
-                    if (error) {
-                        res.json(error)
-                    } else {
-                        res.json(req.body)
-                    }
-                }
-            )
-        }
-    } else {
-        res.render('forbidden');
-    }
-
-})
-router.post('/login', (req, res, next) => {
-    sess = req.session;
-    if (!sess.username) {
-        User.login(req.body,
+    debug('request parameters ' + req.params)
+    debug('session in request: ' + session)
+    session = req.session;
+    if (session.username && req.params.id && session.id == req.params.id) {
+        User.getUserById(req.params.id,
             data => {
-                if (Object.keys(data[0]).length) {
-                    sess.username = data[0].username;
-                    res.redirect('http://localhost:8080')
-                    res.status(200)
-                    res.end()
+                if (data) {
+                    res.json(data)
                 } else {
-                    res.redirect('http://localhost:8080')
-                    res.status(400)
-                    res.end()
+                    res.json(req.body)
                 }
             },
             error => {
                 if (error) {
-                    if (error.code === 'ER_DUP_ENTRY') // check ER_DUP_ENTRY
-                    { res.status(409) } else { res.status(400) }
                     res.json(error)
-                } else { res.json(req.body) }
-            })
-    } else { res.redirect('forbidden') }
+                    res.status(error.code)
+                } else {
+                    res.json(req.body)
+                }
+            }
+        )
+    } else if (session.username && session.role === 'admin') {
+        User.getAllUsers(
+            data => {
+                if (data) {
+                    res.json(data)
+                } else {
+                    res.json(req.body)
+                }
+            },
+            error => {
+                if (error) {
+                    res.json(error)
+                } else {
+                    res.json(req.body)
+                }
+            }
+        )
+    } else {
+        debug('an unlogged user attempted to get users/user')
+        res.status(HttpStatus.FORBIDDEN).render('forbidden');
+    }
+
 })
+
 router.post('/', (req, res, next) => {
     User.addUser(req.body,
         data => {
@@ -91,8 +70,8 @@ router.post('/', (req, res, next) => {
     )
 })
 router.delete('/:id', (req, res, next) => {
-    sess = req.session;
-    if (sess.username) {
+    session = req.session;
+    if (session.username) {
         User.deleteUser(req.params.id,
             data => {
                 if (data) {
@@ -114,26 +93,23 @@ router.delete('/:id', (req, res, next) => {
     }
 })
 router.put('/:id', (req, res, next) => {
-    sess = req.session;
-    if (sess.username) {
-        if (req.body.username && req.body.username === sess.username) {
-            User.updateUser(req.params.id, req.body,
-                data => {
-                    if (data) { res.json(data) } else { res.json(req.body) }
-                },
-                error => {
-                    if (error) {
-                        if (error.code === 'ER_DUP_ENTRY') { // check ER_DUP_ENTRY
-                            res.status(409)
-                        } else { res.status(400) }
-                        res.json(error)
-                    } else { res.json(req.body) }
-                })
-        } else {
-            res.render('forbidden');
-        }
+    session = req.session;
+    if (session.username && req.body.username && req.body.username === session.username) {
+        User.updateUser(req.params.id, req.body,
+            data => {
+                if (data) { res.json(data) } else { res.json(req.body) }
+            },
+            error => {
+                if (error) {
+                    if (error.code === 'ER_DUP_ENTRY') { // check ER_DUP_ENTRY
+                        res.status(409)
+                    } else { res.status(400) }
+                    res.json(error)
+                } else { res.json(req.body) }
+            })
     } else {
         res.render('forbidden');
     }
+
 })
 module.exports = router
