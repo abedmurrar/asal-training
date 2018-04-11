7d07420b9c0054a9a15988a0c5afdeeae80d8f3b
abedmurrar@hamzah-pc <abedmurrar15@gmail.com>
2018-04-11 21:45:19 +0300

    beautify code and use mongodb models

--- node/routes/users.js
+++ node/routes/users.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/User')
+//const User = require('../models/mysql/User')
+const User = require('../models/mongodb/User')
 var HttpStatus = require('http-status-codes') // http status codes package
 var session
 
 router.get('/:id?', (req, res) => {
   session = req.session
-  if (session.username && req.params.id && (session.id === req.params.id || session.role === 'admin')) {
+  if (session.username && req.params.id && (session.uid === req.params.id || session.role === 'admin')) {
     User.getUserById(req.params.id,
-      data => {
-        if (data) {
-          res.status(HttpStatus.OK).json(data)
+      user => {
+        if (user) {
+          res.status(HttpStatus.OK).json(user)
         } else {
           res.status(HttpStatus.NO_CONTENT).json(req.body)
         }
@@ -25,9 +26,9 @@ router.get('/:id?', (req, res) => {
     )
   } else if (session.username && session.role === 'admin') {
     User.getAllUsers(
-      data => {
-        if (data) {
-          res.status(HttpStatus.OK).json(data)
+      users => {
+        if (users) {
+          res.status(HttpStatus.OK).json(users)
         } else {
           res.status(HttpStatus.NO_CONTENT).json(req.body)
         }
@@ -41,15 +42,24 @@ router.get('/:id?', (req, res) => {
       }
     )
   } else {
-    res.status(HttpStatus.FORBIDDEN).json({ success: false, message: 'forbidden' })
+    res.status(HttpStatus.FORBIDDEN).json({
+      success: false,
+      message: 'forbidden'
+    })
   }
 })
 
 router.post('/', (req, res) => {
   User.addUser(req.body,
-    data => {
-      if (data.length > 0) {
-        res.status(HttpStatus.OK).json({ success: true, message: 'Registered successfully', id: data[0] })
+    addedUser => {
+      // if (data.length > 0) { //mysql 
+      if (Object.keys(addedUser).length > 0) { // mongodb 
+        res.status(HttpStatus.OK).json({
+          success: true,
+          message: 'Registered successfully',
+          id: addedUser._id // mongodb 
+          // id: addedUser[0] //mysql 
+        })
       } else {
         res.status(HttpStatus.NOT_IMPLEMENTED).json(req.body)
       }
@@ -57,12 +67,29 @@ router.post('/', (req, res) => {
     error => {
       if (error) {
         if (error.code === 'ER_DUP_ENTRY') {
-          if (error.sqlMessage.includes('username')) { res.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.sqlMessage, username: 'Username already exists' }) } else if (error.sqlMessage.includes('email')) { res.status(409).json({ success: false, message: error.sqlMessage, email: 'Email already registered' }) }
+          if (error.sqlMessage.includes('username')) {
+            res.status(HttpStatus.BAD_REQUEST).json({
+              success: false,
+              message: error.sqlMessage,
+              username: 'Username already exists'
+            })
+          } else if (error.sqlMessage.includes('email')) {
+            res.status(409).json({
+              success: false,
+              message: error.sqlMessage,
+              email: 'Email already registered'
+            })
+          }
         } else {
           error.success = false
           res.status(HttpStatus.BAD_REQUEST).json(error)
         }
-      } else { res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ success: false, message: 'missing parameter' }) }
+      } else {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          success: false,
+          message: 'missing parameter'
+        })
+      }
     }
   )
 })
@@ -88,7 +115,10 @@ router.delete('/:id', (req, res) => {
         }
       })
   } else {
-    res.status(HttpStatus.FORBIDDEN).json({ success: false, message: 'forbidden' })
+    res.status(HttpStatus.FORBIDDEN).json({
+      success: false,
+      message: 'forbidden'
+    })
   }
 })
 router.put('/:id', (req, res) => {
@@ -100,21 +130,47 @@ router.put('/:id', (req, res) => {
           session.username = (req.body.username && req.body.username.length > 0) ? req.body.username : session.username
           session.pass = (req.body.password && req.body.password.length > 0) ? req.body.password : session.pass
           session.email = (req.body.email && req.body.email.length > 0) ? req.body.email : session.email
-          res.status(HttpStatus.OK).json({ success: true, message: 'User modified', status: data })
-        } else { res.status(HttpStatus.NOT_MODIFIED).json(req.body) }
+          res.status(HttpStatus.OK).json({
+            success: true,
+            message: 'User modified',
+            status: data
+          })
+        } else {
+          res.status(HttpStatus.NOT_MODIFIED).json(req.body)
+        }
       },
       error => {
         if (error) {
           if (error.code === 'ER_DUP_ENTRY') {
-            if (error.sqlMessage.includes('username')) { res.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.sqlMessage, username: 'Username already exists' }) } else if (error.sqlMessage.includes('email')) { res.status(409).json({ success: false, message: error.sqlMessage, email: 'Email already registered' }) }
+            if (error.sqlMessage.includes('username')) {
+              res.status(HttpStatus.BAD_REQUEST).json({
+                success: false,
+                message: error.sqlMessage,
+                username: 'Username already exists'
+              })
+            } else if (error.sqlMessage.includes('email')) {
+              res.status(409).json({
+                success: false,
+                message: error.sqlMessage,
+                email: 'Email already registered'
+              })
+            }
           } else {
             error.success = false
             res.status(HttpStatus.BAD_REQUEST).json(error)
           }
-        } else { res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ success: false, message: 'Not allowed' }) }
+        } else {
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: 'Not allowed'
+          })
+        }
       })
   } else {
-    res.status(HttpStatus.FORBIDDEN).json({ success: false, message: 'forbidden' })
+    res.status(HttpStatus.FORBIDDEN).json({
+      success: false,
+      message: 'forbidden'
+    })
   }
 })
 module.exports = router
