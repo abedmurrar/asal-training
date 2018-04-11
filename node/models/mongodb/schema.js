2f9de0aa6f86f7c57eaa3dd9e46d267b8ac738ab
abedmurrar@hamzah-pc <abedmurrar15@gmail.com>
2018-04-11 21:38:39 +0300

    move models depending on their controller

--- node/models/mongodb/schema.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('../../config_mysql')
-// var uniqueValidator = require('mongoose-unique-validator')
-
-
-// usersSchema.plugin(uniqueValidator, { message: 'is already taken' })
-
